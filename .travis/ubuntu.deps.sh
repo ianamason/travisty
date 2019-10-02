1433e64d155dc0a00e03a7a641b2a752216f8976
Ian A. Mason <iam@csl.sri.com>
2019-10-02 15:42:40 -0700

    OK now to actually see if we can build something, II

--- .travis/ubuntu.deps.sh
+++ .travis/ubuntu.deps.sh
@@ -2,10 +2,11 @@
 
 sudo apt-get install curl cmake make libgmp-dev
 
-echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile
-
-source ${HOME}/.bash_profile
-
-pyenv global 3.6.7
-
 echo $PYTHON
+
+if [ "$PYTHON" = "3.6.7" ];
+then
+    echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile;
+    source ${HOME}/.bash_profile;
+    pyenv global 3.6.7;
+fi

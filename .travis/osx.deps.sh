a281ec87df59af0af8e8358c43d987b565e5d465
Ian A. Mason <iam@csl.sri.com>
2019-10-02 16:08:22 -0700

    OK so still picky...

--- .travis/osx.deps.sh
+++ .travis/osx.deps.sh
@@ -2,17 +2,14 @@
 
 brew outdated cmake || brew upgrade cmake
 brew outdated gmp || brew upgrade gmp
+brew outdated pyenv || brew upgrade pyenv;
 
 echo $PYTHON
 
-if [ "$PYTHON" = "3.6.7" ];
-then
-    brew outdated pyenv || brew upgrade pyenv;
-    echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile;
-    source ${HOME}/.bash_profile;
-    # no python 3.6.7 on osx as of 10/2019;
-    pyenv install 3.6.7;
-    pyenv global 3.6.7;
-else
-    touch ${HOME}/.bash_profile;
-fi
+
+echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile;
+source ${HOME}/.bash_profile;
+
+# no python 3.6.7 on osx as of 10/2019;
+pyenv install ${PYTHON};
+pyenv global ${PYTHON};
--- .travis/ubuntu.deps.sh
+++ .travis/ubuntu.deps.sh
@@ -4,9 +4,8 @@ sudo apt-get install curl cmake make libgmp-dev
 
 echo $PYTHON
 
-if [ "$PYTHON" = "3.6.7" ];
-then
-    echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile;
-    source ${HOME}/.bash_profile;
-    pyenv global 3.6.7;
-fi
+echo 'eval "$(pyenv init -)"' >> ${HOME}/.bash_profile;
+source ${HOME}/.bash_profile;
+
+pyenv install ${PYTHON};
+pyenv global ${PYTHON};
